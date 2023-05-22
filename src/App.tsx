495f37a777796c2a0629e40c88551cf3b8c54203
Mariane Anjos <100323487+MarianeAnjos@users.noreply.github.com>
2023-05-22 14:37:59 -0300

    Criação de rotas com Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import './App.css'
-import { Grid } from '@material-ui/core'; 
 import Home from './paginas/home/Home'
+import Login from './paginas/login/Login';
 
 function App(){
   return(
-    <>
-    <Navbar />
-    <Home/>
-    <Footer/>
-    </>
+    <BrowserRouter>
+      <Navbar/>
+        <div>
+      <Routes>
+          <Route path='/home' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+      </Routes>
+        </div>
+      <Footer/>
+  </BrowserRouter>
+
   )
 }
 
---
+++ src/paginas/login/Login.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Login.css'
+import { Box, Grid } from '@mui/material';
+
+function Login() {
+  return (
+    <Grid container>
+        <Grid xs={6}>
+            <Box>
+                <form>
+
+                </form>
+            </Box>
+
+        </Grid>
+        <Grid xs={6}>
+        </Grid>
+    </Grid>
+
+  )
+}
+
+export default Login
