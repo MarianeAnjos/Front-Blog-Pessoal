ffb3caa21dbf3f931e4ca2f7d77c33e76fa0065f
Mariane Anjos <100323487+MarianeAnjos@users.noreply.github.com>
2023-05-22 20:26:34 -0300

    Mudança da estilização inLine para página CSS

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
 import {Box} from '@mui/material';
 import{ Link } from 'react-router-dom';
@@ -9,35 +8,35 @@ function Navbar() {
         <>
             <AppBar position="static">
                 <Toolbar variant="dense">
-                    <Box style={{ cursor: "pointer" }} >
+                    <Box className='cursor' >
                         <Typography variant="h5" color="inherit">
                             Conte-meUmItan
                         </Typography>
                     </Box>
 
                     <Box display="flex" justifyContent="start">
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
                                 Home
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
                                 Itans
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
                                 Temas
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
                                 Conte Seu Itan
                             </Typography>
                         </Box>
                         <Link to='/login' className='text-decorator-none'>
-                        <Box mx={1} style={{ cursor: "pointer", color:'white'}}>
+                        <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
                                 logout
                             </Typography>
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -6,22 +6,22 @@ import './Home.css';
 function Home() {
     return (
         <>
-            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
+            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
                 <Grid alignItems="center" item xs={6}>
                     <Box paddingX={20} >
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>Venha aprender e nos ensinar um pouco pais sobre a Mitologia Iorubá!</Typography>
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>Venha aprender e nos ensinar um pouco pais sobre a Mitologia Iorubá!</Typography>
                     </Box>
                     <Box display="flex" justifyContent="center">
                         <Box marginRight={1}>
                         </Box>
-                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                        <Button variant="outlined" className='botao'>Ver Postagens</Button>
                     </Box>
                 </Grid>
                 <Grid item xs={6} >
                     <img src="https://media.licdn.com/dms/image/D4D22AQFdw0TRjmP_rQ/feedshare-shrink_1280/0/1683548739890?e=1687392000&v=beta&t=A6KQhA0iXZdDm9hVI741_PQcRAmWIEluOmmnQy6-Q54" alt="" width="710px" height="700px" />
                 </Grid>
-                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                <Grid xs={12} className="postagens">
                 </Grid>
             </Grid>
         </>
