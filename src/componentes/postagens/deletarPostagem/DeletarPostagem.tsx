3621381aace2e431addfdfb6b2194ee69e794936
Mariane Anjos <100323487+MarianeAnjos@users.noreply.github.com>
2023-06-01 17:23:21 -0300

    Update DeletarPostagem.tsx

--- src/componentes/postagens/deletarPostagem/DeletarPostagem.tsx
+++ src/componentes/postagens/deletarPostagem/DeletarPostagem.tsx
@@ -7,7 +7,7 @@ import { useSelector } from 'react-redux';
 import { TokenState } from '../../../store/tokens/tokensReducer';
 
 
-toast,
+
 function DeletarPostagem() {
   const token = useSelector<TokenState, TokenState["tokens"]>(
     (state) => state.tokens
